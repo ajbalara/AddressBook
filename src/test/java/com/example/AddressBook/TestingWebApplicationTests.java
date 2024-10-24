5ee9090408c52d270a3386bf4cf112aca131387c
Anand Balaram <anandbalaram@cmail.carleton.ca>
2024-10-24 16:25:20 -0400

    Add some comments

--- src/test/java/com/example/AddressBook/TestingWebApplicationTests.java
+++ src/test/java/com/example/AddressBook/TestingWebApplicationTests.java
@@ -1,10 +1,32 @@
 package com.example.AddressBook;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(WebController.class)
 class TestingWebApplicationTests {
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
     void contextLoads() {}
+
+    /*
+    @Test
+    void greetingShouldReturnMessageFromService() throws Exception {
+        when(service.greet()).thenReturn("Hello, Mock");
+        this.mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello, Mock")));
+    }
+     */
 }
